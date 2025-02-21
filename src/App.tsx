d4bdc093928d1bc945f278136e9ad846eb4d5f10
Marcin Kaczor <marcin.kaczor96@gmail.com>
2025-02-22 00:13:46 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex tuscany-1'>
+        <h1 className='title'>Kasia i Marcin</h1>
+      </div>
+      <div className='box content'>
+        
+      </div>
+      <div className='box tuscany-2'>
+      
+      </div>
+      <div className='box content'>
+      
+      </div>
+      <div className='box tuscany-3'>
+      
+      </div>
+    </>
+  )
+}
+
+export default App
